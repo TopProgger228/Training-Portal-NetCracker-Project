cd76ffe5cb45e8b5707455e3063cfed9e85a5cb7
Maksym Didyk <Maksym_didyk97_ua@rambler.ru>
2019-04-15 17:18:17 +0300

    Add email sender

---
+++ backend/src/main/java/com/group3/basic/netcracker/backend/MailSender/Contact.java
@@ -0,0 +1,9 @@
+package com.group3.basic.netcracker.backend.MailSender;
+
+public class Contact {
+    public static final String MY_EMAIL = "Mopozkolol@gmail.com";
+
+    public static final String MY_PASSWORD = "Trol2016WTF";
+
+    public static final String FRIEND_EMAIL = "maksymdidykua@gmail.com";
+}
