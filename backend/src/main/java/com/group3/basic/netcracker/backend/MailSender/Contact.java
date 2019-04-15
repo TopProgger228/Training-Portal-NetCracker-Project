a6b5ec9b811b6eb3992da4ac34402dc7bb046077
Maksym Didyk <Maksym_didyk97_ua@rambler.ru>
2019-04-15 17:22:54 +0300

    Update log and pass

--- backend/src/main/java/com/group3/basic/netcracker/backend/MailSender/Contact.java
+++ backend/src/main/java/com/group3/basic/netcracker/backend/MailSender/Contact.java
@@ -1,9 +1,9 @@
 package com.group3.basic.netcracker.backend.MailSender;
 
 public class Contact {
-    public static final String MY_EMAIL = "Mopozkolol@gmail.com";
+    public static final String MY_EMAIL = "Email";
 
-    public static final String MY_PASSWORD = "Trol2016WTF";
+    public static final String MY_PASSWORD = "Password";
 
-    public static final String FRIEND_EMAIL = "maksymdidykua@gmail.com";
+    public static final String FRIEND_EMAIL = "FriendEmail";
 }
