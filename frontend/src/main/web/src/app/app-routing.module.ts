aec6f6bc89a55454abdcb0233ff18d31ea99b274
testuser <testuser@example.com>
2019-04-15 22:52:52 +0300

    add routing and module for page

--- frontend/src/main/web/src/app/app-routing.module.ts
+++ frontend/src/main/web/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { StartComponent } from './first-page/start/start.component';
+
+const routes: Routes = [
+  {
+    path: 'firstPage',
+    component: StartComponent
+  },
+  {
+    path: '',
+    redirectTo: 'firstPage',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
