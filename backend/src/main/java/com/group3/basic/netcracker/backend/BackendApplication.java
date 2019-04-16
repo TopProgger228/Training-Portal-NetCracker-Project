b87892ec1c359113e6625e71eb901a6513366f8a
MAX-DESKTOP <max292009@gmail.com>
2019-04-16 11:50:45 +0300

    Totally removed Controller. Changed everything to JDBC

--- backend/src/main/java/com/group3/basic/netcracker/backend/BackendApplication.java
+++ backend/src/main/java/com/group3/basic/netcracker/backend/BackendApplication.java
@@ -1,13 +1,47 @@
 package com.group3.basic.netcracker.backend;
 
+import com.group3.basic.netcracker.backend.UsersTable.dao.jdbc.JdbcTemplateUsersDaoImpl;
+import com.group3.basic.netcracker.backend.UsersTable.model.Users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+        ApplicationContext context = new ClassPathXmlApplicationContext("jdbctemplate-users-config.xml");
+
+        JdbcTemplateUsersDaoImpl jdbcTemplateUsersDao =
+                (JdbcTemplateUsersDaoImpl) context.getBean("jdbcTemplateUsersDao");
+        LocalDate todayDate = LocalDate.now();
+
+        System.out.println("========Creating new records to DB========");
+        jdbcTemplateUsersDao.createUser("student0", 4,"Jack", "Bo", "mail@temp-mail.com", "12345", todayDate, null,5);
+        //jdbcTemplateUsersDao.createUser("Petr", "C++ Developer", 2);
+        //jdbcTemplateUsersDao.createUser("DesignerAsya", "UI Developer", 1);
+
+        System.out.println("========Developers List========");
+        List<Users> user = jdbcTemplateUsersDao.listUsers();
+        for (Users users : user) {
+            System.out.println(users);
+        }
+
+        System.out.println("========Some changes to DB========");
+        //jdbcTemplateUsersDao.updateUser(33, "DesignerAsya", "UI Developer", 2);
+        //jdbcTemplateUsersDao.removeUser(32);
+
+        System.out.println("========Final Developers List========");
+        List<Users> finalUsers = jdbcTemplateUsersDao.listUsers();
+
+        for (Users users : finalUsers) {
+            System.out.println(users);
+        }
     }
 
 }
---
+++ backend/src/main/java/com/group3/basic/netcracker/backend/UsersTable/dao/jdbc/JdbcTemplateUsersDaoImpl.java
@@ -0,0 +1,58 @@
+package com.group3.basic.netcracker.backend.UsersTable.dao.jdbc;
+
+import com.group3.basic.netcracker.backend.UsersTable.dao.UsersDAO;
+import com.group3.basic.netcracker.backend.UsersTable.model.Users;
+import com.group3.basic.netcracker.backend.UsersTable.util.UsersMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.time.LocalDate;
+import java.util.List;
+
+public class JdbcTemplateUsersDaoImpl implements UsersDAO {
+    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public void createUser(String username, int role_id, String fname, String lname, String email, String pass, LocalDate created_at, byte[] photo, int id) {
+        String SQL = "INSERT INTO Users (username, role_id, fname, lname, email, pass, created_at,photo, id) VALUES (?,?,?,?,?,?,?,?,?)";
+
+        jdbcTemplate.update(SQL, username, role_id, fname, lname, email, pass, created_at, photo, id);
+        System.out.println("Users successfully created.\nUsername: " + username + ";\nFirst name: " +
+                fname + ";\nLast name: " + lname + "\nEmail: " + email + "\nPassword: " + pass + "\nCreated at: " + created_at + "\nPhoto: " + photo + "\nId: " + id);
+    }
+
+    @Override
+    public Users getUserById(int id) {
+        String SQL = "SELECT * FROM Users WHERE id = ?";
+        Users users = (Users) jdbcTemplate.queryForObject(SQL, new Object[]{id}, new UsersMapper());
+        return users;
+    }
+
+    @Override
+    public List listUsers() {
+        String SQL = "SELECT * FROM Users";
+        List Users = jdbcTemplate.query(SQL, new UsersMapper());
+        return Users;
+    }
+
+    @Override
+    public void removeUser(int id) {
+        String SQL = "DELETE FROM Users WHERE id = ?";
+        jdbcTemplate.update(SQL, id);
+        System.out.println("Users with id: " + id + " successfully removed");
+    }
+
+    @Override
+    public void updateUser(String username, int role_id, String fname, String lname, String email, String pass, LocalDate created_at, byte[] photo, int id) {
+        String SQL = "UPDATE Users SET username = ?, role_id = ?, fname = ?, lname = ?, email = ?, pass = ?, created_at = ?, photo = ? WHERE id = ?";
+        jdbcTemplate.update(SQL, username, role_id, fname, lname, email, pass, created_at, photo, id);
+        System.out.println("Users with id: " + id + " successfully updated.");
+    }
+}
