4a7db7a846bb30c2176bbf1682497e81380522f3
testuser <testuser@example.com>
2019-04-16 16:33:55 +0300

    Sign in and sign up serveces added, UI will be upgrade.

--- frontend/src/main/web/src/app/app-routing.module.ts
+++ frontend/src/main/web/src/app/app-routing.module.ts
@@ -3,7 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 import { StartComponent } from './first-page/start/start.component';
-import { SignInComponent } from './sign-in/sign-in.component';
+import { RegisterComponent } from './register/register.component';
+import { SingInComponent } from './sign-in/sign-in.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { MngComponent } from './mng/mng.component';
+import { TrainerComponent } from './trainer/trainer.component';
 
 const routes: Routes = [
   {
@@ -11,14 +16,34 @@ const routes: Routes = [
     component: StartComponent
   },
   {
+    path: 'user',
+    component: UserComponent
+  },
+  {
+    path: 'mng',
+    component: MngComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  },
+  {
+    path: 'trainer',
+    component: TrainerComponent
+  },
+  {
+    path: 'auth/login',
+    component: SingInComponent
+  },
+  {
+    path: 'signup',
+    component: RegisterComponent
+  },
+  {
     path: '',
     redirectTo: 'firstPage',
     pathMatch: 'full'
   }
-  // {
-  //   path: 'sign-in',
-  //   component: SignInComponent
-  // }
 ];
 
 @NgModule({
--- frontend/src/main/web/src/app/shared/user.model.ts
+++
@@ -1,7 +0,0 @@
-export class User {
-    UserName : String;
-    Password : String;
-    Email : String;
-    FirstName : String;
-    LastName : String;
-}
