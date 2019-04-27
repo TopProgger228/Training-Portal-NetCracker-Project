30fca1ea2821193a75c0d77cbcd5fb92f941c363
TopProgger228 <36416380+TopProgger228@users.noreply.github.com>
2019-04-27 16:28:20 +0300

    Delete Contact.java

--- backend/src/main/java/com/group3/basic/netcracker/backend/mailsender/Contact.java
+++
@@ -1,9 +0,0 @@
-package com.group3.basic.netcracker.backend.mailsender;
-
-public class Contact {
-    public static final String MY_EMAIL = "Email";
-
-    public static final String MY_PASSWORD = "Password";
-
-    public static final String FRIEND_EMAIL = "FriendEmail";
-}
