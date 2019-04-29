6a92a0318ce5e65c7f7b0b7c204521f66a6754a5
ilyaShesterikov <shesterikov9@gmail.com>
2019-04-29 19:36:07 +0300

    Errors fixed

--- frontend/src/main/web/src/app/users-info/users-info.component.spec.ts
+++ frontend/src/main/web/src/app/users-info/users-info.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsersInfoComponent } from './users-info.component';
 
-describe('CoursesInfoComponent', () => {
+describe('UsersInfoComponent', () => {
   let component: UsersInfoComponent;
   let fixture: ComponentFixture<UsersInfoComponent>;
 
