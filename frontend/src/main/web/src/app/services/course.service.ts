1f73291c5b617d609b9546b5db0ab37502a4eca8
MarkusEfr <markus.efr@gmail.com>
2019-05-07 23:27:56 +0300

    Try to get response for download xls report from angular. Add http request

--- frontend/src/main/web/src/app/services/course.service.ts
+++ frontend/src/main/web/src/app/services/course.service.ts
@@ -3,7 +3,7 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Courses} from "../groups-schedule/courses";
 import {Observable} from "rxjs";
 import {Course} from "./course";
-
+import { ResponseContentType } from '@angular/http';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
@@ -17,6 +17,8 @@ export class CourseService {
   private coursesUrl = 'http://localhost:8080/api/coursesinfo/getcourses?username=';
   private courseListUrl = "http://localhost:8080/api/courses-list"
   private courseLastTenListUrl = "http://localhost:8080/api/last-ten-courses-list";
+  private attendanceReportUrl = 'http://localhost:8080/api/attendanceExcel'
+  private sessionService: any;
 
   constructor(private http: HttpClient) {
   }
@@ -24,13 +26,22 @@ export class CourseService {
   createNewCourse(newCourse: Courses): Observable<any> {
     return this.http.post<string>(this.createCourseUrl, newCourse, httpOptions);
   }
+
   getCourses(username: string): Observable<any> {
     return this.http.get<Course>(this.coursesUrl + username, httpOptions);
   }
-  getCoursesList():Observable<any>{
+
+  getCoursesList(): Observable<any> {
     return this.http.get<Course>(this.courseListUrl, httpOptions);
   }
-  getLastTenCoursesList():Observable<any>{
+
+  getLastTenCoursesList(): Observable<any> {
     return this.http.get<Course>(this.courseLastTenListUrl, httpOptions);
+  }
+  attendanceExcel(): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'responseType':  'ResponseContentType.Blob',
+        'Content-Type':  'application/vnd.ms-excel'})};
+    return this.http.get(this.attendanceReportUrl, httpOptions);
   }
 }
